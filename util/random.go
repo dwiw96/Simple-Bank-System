692ca428e5004637b07a94e2d0dea472d0defe9d
dwiw96 <dwiwahyudi1996@gmail.com>
2023-06-08 15:34:49 +0700

    Add RandomEmail() func

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -48,4 +49,8 @@ func RandomCurrency() string {
 	currencies := []string{"IDR", "USD", "EUR"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
