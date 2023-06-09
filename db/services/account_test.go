27362e63787ac5e767c1c52db63f8d40c95462b4
dwiw96 <dwiwahyudi1996@gmail.com>
2023-06-09 22:17:06 +0700

    change t.Fatalf to t.Errorf

--- db/services/account_test.go
+++ db/services/account_test.go
@@ -31,17 +31,17 @@ func TestCreateAccount(t *testing.T) {
 	account, input := createRandomAccount(t)
 
 	if account.Owner != input.Owner {
-		t.Fatalf("owner name in database \"%s\" isn't same as input \"%s\"", account.Owner, input.Owner)
+		t.Errorf("owner name in database \"%s\" isn't same as input \"%s\"", account.Owner, input.Owner)
 	} else if account.Balance != input.Balance {
-		t.Fatalf("Balance in database \"%d\" isn't same as input \"%d\"", account.Balance, input.Balance)
+		t.Errorf("Balance in database \"%d\" isn't same as input \"%d\"", account.Balance, input.Balance)
 	} else if account.Currency != input.Currency {
-		t.Fatalf("Currency in database \"%s\" isn't same as input \"%s\"", account.Currency, input.Currency)
+		t.Errorf("Currency in database \"%s\" isn't same as input \"%s\"", account.Currency, input.Currency)
 	}
 
 	if account.ID == 0 {
-		t.Fatalf("ID isn't automatically generate")
+		t.Errorf("ID isn't automatically generate")
 	} else if account.CreatedAt.IsZero() == true {
-		t.Fatalf("created_at is nill")
+		t.Errorf("created_at is nill")
 	}
 }
 
@@ -53,17 +53,17 @@ func TestGetAccount(t *testing.T) {
 		t.Fatal(err)
 	}
 	if account2 == nil {
-		t.Fatalf("Can't read account from database")
+		t.Error("Can't read account from database")
 	}
 
 	if account1.ID != account2.ID {
-		t.Fatalf("ID input \"%d\" != ID output \"%d\"", account1.ID, account2.ID)
+		t.Errorf("ID input \"%d\" != ID output \"%d\"", account1.ID, account2.ID)
 	} else if account2.Owner != account2.Owner {
-		t.Fatalf("Owner input \"%s\" != Owner output \"%s\"", account1.Owner, account2.Owner)
+		t.Errorf("Owner input \"%s\" != Owner output \"%s\"", account1.Owner, account2.Owner)
 	} else if account1.Balance != account2.Balance {
-		t.Fatalf("Balance input \"%d\" != Balance output \"%d\"", account1.Balance, account2.Balance)
+		t.Errorf("Balance input \"%d\" != Balance output \"%d\"", account1.Balance, account2.Balance)
 	} else if account1.Currency != account2.Currency {
-		t.Fatalf("Currency input \"%s\" != Currency output \"%s\"", account1.Currency, account2.Currency)
+		t.Errorf("Currency input \"%s\" != Currency output \"%s\"", account1.Currency, account2.Currency)
 	}
 }
 
@@ -81,37 +81,37 @@ func TestUpdateAccount(t *testing.T) {
 
 	account2, err := testQueries.GetAccount(ctx, input.ID)
 	if err != nil {
-		t.Fatalf("GetAccount() function failed = %s", err)
+		t.Errorf("GetAccount() function failed = %s", err)
 	} else if account2 == nil {
-		t.Fatalf("failed to get an account from database")
+		t.Errorf("failed to get an account from database")
 	}
 
 	if account1.ID != account2.ID {
-		t.Fatalf("ID input \"%d\" != ID output \"%d\"", account1.ID, account2.ID)
+		t.Errorf("ID input \"%d\" != ID output \"%d\"", account1.ID, account2.ID)
 	} else if account2.Owner != account2.Owner {
-		t.Fatalf("Owner input \"%s\" != Owner output \"%s\"", account1.Owner, account2.Owner)
+		t.Errorf("Owner input \"%s\" != Owner output \"%s\"", account1.Owner, account2.Owner)
 	} else if account1.Balance == account2.Balance {
-		t.Fatalf("Balance input \"%d\" isn't updated, Balance output \"%d\"", account1.Balance, account2.Balance)
+		t.Errorf("Balance input \"%d\" isn't updated, Balance output \"%d\"", account1.Balance, account2.Balance)
 	} else if account1.Currency != account2.Currency {
-		t.Fatalf("Currency input \"%s\" != Currency output \"%s\"", account1.Currency, account2.Currency)
+		t.Errorf("Currency input \"%s\" != Currency output \"%s\"", account1.Currency, account2.Currency)
 	}
 }
 
 func TestDeleteAccount(t *testing.T) {
 	account1, _ := createRandomAccount(t)
 	if account1 == (pkg.Account{}) {
-		t.Fatalf("failed to create random account")
+		t.Errorf("failed to create random account")
 	}
 	err := testQueries.DeleteAccount(ctx, account1.ID)
 	if err != nil {
-		t.Fatalf("DeleteAccount() err : %s", err)
+		t.Errorf("DeleteAccount() err : %s", err)
 	}
 
 	account2, err := testQueries.GetAccount(ctx, account1.ID)
 	if errors.Is(err, sql.ErrNoRows) {
-		t.Fatalf("Account still not deleted, %s", sql.ErrNoRows)
+		t.Errorf("Account still not deleted, %s", sql.ErrNoRows)
 	} else if account2 != nil {
-		t.Fatalf("Account still not deleted")
+		t.Errorf("Account still not deleted")
 	}
 }
 
@@ -133,7 +133,7 @@ func TestListAccount(t *testing.T) {
 
 	for _, account := range accounts {
 		if account == (pkg.Account{}) {
-			t.Fatalf("the accounts are empty")
+			t.Errorf("the accounts are empty")
 		}
 	}
 }
