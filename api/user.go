4759c57ab02a76b480f4e26af0fb533d1243752c
dwiw96 <dwiwahyudi1996@gmail.com>
2023-07-03 17:12:39 +0700

    Added route func for login

--- api/user.go
+++ api/user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"simple-bank-system/db/pkg"
 	"simple-bank-system/db/services"
 	"simple-bank-system/util"
 
@@ -22,12 +23,24 @@ type createUserRequest struct {
 	Email          string `json:"email" validate:"required,email"`
 }
 
-type createUserResponse struct {
+type userResponse struct {
 	Username         string
 	FullName         string
 	Email            string
 	PasswordChangeAt time.Time
 	CreatedAt        time.Time
+}
+
+// Create func to pass user data in separated func from 'createUser()'
+// because it has pasword data
+func newUserResponse(user *pkg.User) userResponse {
+	return userResponse{
+		Username:         user.Username,
+		FullName:         user.FullName,
+		Email:            user.Email,
+		PasswordChangeAt: user.PasswordChangeAt,
+		CreatedAt:        user.CreatedAt,
+	}
 }
 
 func (server *Server) createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -84,13 +97,69 @@ func (server *Server) createUser(w http.ResponseWriter, r *http.Request, _ httpr
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
-	response := createUserResponse{
-		Username:         user.Username,
-		FullName:         user.FullName,
-		Email:            user.Email,
-		PasswordChangeAt: user.PasswordChangeAt,
-		CreatedAt:        user.CreatedAt,
+	response := newUserResponse(user)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+type loginRequest struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type loginResponse struct {
+	AccessToken string       `json:"access_token"`
+	User        userResponse `json:"user"`
+}
+
+func (server *Server) loginUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Println("(1) Pass")
+	var req loginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		fmt.Println("----------- (1)")
+		http.Error(w, "Failed to decode login data", http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err.Error())
+		return
 	}
+	fmt.Println("(2) Pass")
+	fmt.Println("request:")
+	fmt.Println(req)
+	user, err := server.store.GetUser(server.ctx, req.Username)
+	if err != nil {
+		fmt.Println("----------- (2)")
+		http.Error(w, "user with input username doesn't exist", (http.StatusBadRequest))
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	fmt.Println("(3) Pass")
+	fmt.Println("Get User: ")
+	fmt.Println(user)
+	err = util.VerifyPassword(req.Password, user.HashedPassword)
+	fmt.Println("(3) Check")
+	if err != nil {
+		fmt.Println("----------- (3)")
+		http.Error(w, "Your password is wrong", http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+	fmt.Println("(4) Pass")
+	token, err := server.tokenMaker.CreateToken(req.Username, server.duration)
+	if err != nil {
+		fmt.Println("----------- (4)")
+		http.Error(w, "Failed to create encryted token", http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	fmt.Println("(5) Pass")
+	response := loginResponse{
+		AccessToken: token,
+		User:        newUserResponse(user),
+	}
+
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
